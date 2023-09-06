38db2305b7c08a5487d715ff9d80241b6509077b
Keizou Wang <keizouw@davidsonacademy.unr.edu>
2023-09-06 15:18:21 -0700

    jccn landing page and basic styling

--- src/index.js
+++ src/index.js
@@ -12,15 +12,12 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'sites/landing/preload.js'),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile(path.join(__dirname, 'sites/landing/index.html'));
 };
 
 // This method will be called when Electron has finished
